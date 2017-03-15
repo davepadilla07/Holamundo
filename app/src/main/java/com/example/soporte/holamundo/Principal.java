54b34b16762a69f5f7dc3a036dbc9922fbd2c755
Roberto Morales <roberto@morales.com.co>
2017-03-14 21:13:01 -0500

    Aplicacion version 1.0

--- app/src/main/java/com/example/soporte/holamundo/Principal.java
+++ app/src/main/java/com/example/soporte/holamundo/Principal.java
@@ -1,32 +1,66 @@
 package com.example.soporte.holamundo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Principal extends AppCompatActivity {
-
+    //Declaramos variables
     private EditText cajaNombre;
-    private TextView cajaMensaje;
+    private EditText cajaApellido;
+    private Intent i;
+    private Bundle b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
 
+        //Capturamos las cajas
         cajaNombre = (EditText)findViewById(R.id.txtNombre);
-       // cajaMensaje = (TextView)findViewById(R.id.lblMensaje);
+        cajaApellido = (EditText)findViewById(R.id.txtApellido);
+        //Creamos el objeto intent que sirve para pasar de una actividad a otra
+        i = new Intent(this,Saludo.class);
+        //Creamos el objeto Bundle que sirve para encapsular datos
+        b = new Bundle();
+
     }
 
 
     public void saludar(View v){
-        String aux;
-       // aux = cajaNombre.getText().toString();
-        //cajaMensaje.setText(getResources().getString(R.string.parte_saludo)+" "+aux);
+        String nomb, apell;
+      if(validar()) {
+            //Tomo el valor que la persona ingresó tanto en nombre como en apellido
+            nomb = cajaNombre.getText().toString();
+            apell = cajaApellido.getText().toString();
+           //Encapsulo los valores previamente tomados
+            b.putString("Nombre", nomb);
+            b.putString("Apellido", apell);
+            //Le paso al intent todos los datos en forma encapsulada con el bundle
+            i.putExtras(b);
+            //Arranco la actividad que le intent me diga
+            startActivity(i);
+        }
+    }
 
+    public boolean validar(){
+        if(cajaNombre.getText().toString().isEmpty()){
+           cajaNombre.setError(getResources().getString(R.string.error_1));
+            // Toast.makeText(this,"Digite por favor el nombre",Toast.LENGTH_SHORT).show();
+            return false;
+        }
+       if (cajaApellido.getText().toString().isEmpty()){
+           cajaApellido.setError(getResources().getString(R.string.error_2));
+
+           //Toast.makeText(this,"Digite por favor el Apellido",Toast.LENGTH_SHORT).show();
+          return false;
+        }
+        return true;
     }
 }
 
